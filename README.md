571e37f2fa00384222160cdb6b3344559c833077
Fabio Berger <me@fabioberger.com>
2019-06-06 22:35:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Example Mesh WS Client in Javascript
+
+This repo contains a very simple example of using [web3-providers](https://www.npmjs.com/package/web3-providers)'s `WebsocketProvider` as a client for interacting with [0x Mesh](https://github.com/0xProject/0x-mesh)'s WebSocket endpoints.
+
+### Setup
+
+1. Install the dependencies
+
+```
+yarn install
+```
+
+2. Start your Mesh node locally on port `60557` pointing to mainnet (see Docker command in [docs](https://hackmd.io/@fabiob/By4pSjATE))
+
+3. Run WS client
+
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const Web3Providers = require("web3-providers");
+
+const MESH_WS_PORT = 60557;
+const MESH_WS_ENDPOINT = `ws://localhost:${MESH_WS_PORT}`;
+console.log("Mesh WebSocket endpoint: ", MESH_WS_ENDPOINT);
+
+// Instantiate the WebSocket provider/client
+const websocketProvider = new Web3Providers.WebsocketProvider(MESH_WS_ENDPOINT);
+
+// Subscribe to the order events subscription
+console.log("About to subscribe...");
+websocketProvider
+  .subscribe("mesh_subscribe", "orders", [])
+  .then(function(subscriptionId) {
+    console.log("subscriptionId", subscriptionId);
+    // Listen to event on the subscription (topic is the subscriptionId)
+    websocketProvider.on(subscriptionId, function(event) {
+      console.log("Event Received:", JSON.stringify(event, null, "\t"));
+    });
+
+    // Submit an order to the Mesh node
+    console.log("About to send order...");
+    var order = {
+      makerAddress: "0xa3ece5d5b6319fa785efc10d3112769a46c6e149",
+      takerAddress: "0x0000000000000000000000000000000000000000",
+      makerAssetAmount: "100000000000000000000",
+      takerAssetAmount: "100000000000000000000000",
+      expirationTimeSeconds: "1559856615025",
+      makerFee: "0",
+      takerFee: "0",
+      feeRecipientAddress: "0x0000000000000000000000000000000000000000",
+      senderAddress: "0x0000000000000000000000000000000000000000",
+      salt:
+        "46108882540880341679561755865076495033942060608820537332859096815711589201849",
+      makerAssetData:
+        "0xf47261b0000000000000000000000000e41d2489571d322189246dafa5ebde1f4699f498",
+      takerAssetData:
+        "0xf47261b0000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+      exchangeAddress: "0x4f833a24e1f95d70f028921e27040ca56e09ab0b",
+      signature:
+        "0x1c52f75daa4bd2ad9e6e8a7c35adbd089d709e48ae86463f2abfafa3578747fafc264a04d02fa26227e90476d57bca94e24af32f1cc8da444bba21092ca56cd85603"
+    };
+    var payload = {
+      jsonrpc: "2.0",
+      id: 2,
+      method: "mesh_addOrders",
+      params: [[order]]
+    };
+    console.log("mesh_addOrders Payload:", JSON.stringify(payload, null, "\t"));
+    websocketProvider.sendPayload(payload).then(function(response) {
+      console.log(
+        "mesh_addOrders Response: ",
+        JSON.stringify(response, null, "\t")
+      );
+    });
+  })
+  .catch(function(err) {
+    console.log("Error:", err);
+  });
